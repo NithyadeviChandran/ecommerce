a86dfa6dd348e7e11c727c44c39ed0d47f42e057
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-05-19 13:19:33 +0200

    feat:added the hamburger menu

---
+++ main.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById('ham-menu');
+const navMenu = document.getElementById('nav-item');
+console.log('hamburger:',hamburger);
+console.log(`navMenu: ${navMenu}`);
+
+hamburger.addEventListener('click', toggleHamburgerMenu);
+// navMenu.addEventListener('click', toggleHamburgerMenu);
+
+
+function toggleHamburgerMenu(){
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+}
