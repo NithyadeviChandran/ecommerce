ea05383d65b07cb2d9c0dd60adb37592781628a0
NithyadeviChandran <nithyadevi.ch@gmail.com>
2023-05-26 11:19:22 +0200

    feat:added the slider component

--- main.js
+++ main.js
@@ -16,13 +16,39 @@ fetch("https://dummyjson.com/products")
 .then(response=>response.json())
 .then(data=>{
     const products = data.products;
-    const firstProduct= products[27];
-    console.log('FirstProduct:',firstProduct)
-    const productPhotos = firstProduct.images;
-    console.log('Productimage:',productPhotos)
-   
+    const product25= products[25];
+    const product25photos = product25.images;
+    renderSlider(product25photos);
 })
 .catch(error=>{
     console.log(error);
 })
 
+function renderSlider(photos){
+    const slider = document.getElementById('slider');
+    photos.slice(0,4).forEach((photoUrl) => {
+        const image = document.createElement('img');
+        image.src=photoUrl;
+        // console.log(image);
+        slider.appendChild(image);
+    });
+}
+
+const pagination= document.getElementById('pagination');
+pagination.addEventListener('click',handlePaginationClick);
+
+function handlePaginationClick(event){
+const clickedDot = event.target;
+if(clickedDot.classList.contains('page-dot')){
+    const index= parseInt(clickedDot.dataset.index);
+    scrollToIndex(index);
+}
+}
+
+function scrollToIndex(index){
+    const scrollAmount = index*slider.offsetWidth;
+    slider.scrollTo({
+        left:scrollAmount,
+        behaviour: 'smooth'
+    });
+}
